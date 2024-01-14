33a4a59d40ef83c5c9734db270912e53cca52f05
Victoria Mayer <victoriam98@live.com>
2024-01-14 12:10:43 -0600

    Concatenation update

---
+++ test.js
@@ -0,0 +1,8 @@
+//Victoria Mayer    January 14th, 2023
+var hello = 17;
+hello = hello * 4;
+
+var userInput = prompt("What is your favorite color?")
+
+console.log("The value for the variable \"userInput\" is: " + userInput);
+console.log("The variable \"hello\" is equal to: " + hello);
