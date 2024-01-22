5493f5706cb573d478cfdf2166a42ce19d69957f
Victoria Mayer <victoriam98@live.com>
2024-01-21 19:22:06 -0600

    Loop update

---
+++ loop.js
@@ -0,0 +1,15 @@
+//create number variable and set the counter to 0
+var i = 6;
+
+//loop 5 times
+while (i <= 11)
+{
+    //increment variable
+    i++;
+
+    //if odd, print
+    if (i % 2 !==0)
+    {
+        console.log(i);
+    }
+}
